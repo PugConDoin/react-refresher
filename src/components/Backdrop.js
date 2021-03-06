46670b043f4e3709986f0135ccb9b4b3c6058833
Mark Freeman <dorsettri@gmail.com>
2021-03-06 08:04:17 +0000

    add Todo and Backdrop Components with useState and event handling

--- src/components/Backdrop.js
+++ src/components/Backdrop.js
@@ -1,4 +1,5 @@
 function Backdrop(props) {
+  // set the 'onClick' prop to call the 'onCancel' function when the Backdrop 'div' is clicked. We use the onCancel prop when we return the render state for Backdrop in the Todos Component.
   return <div className="backdrop" onClick={props.onCancel}></div>
 }
 
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,18 +4,31 @@ import Modal from './Modal'
 import Backdrop from './Backdrop'
 
 function Todo(props) {
-  // we only want to see the modal, or background, if the delete button is clicked
-  // if open, we want to close both whenever the cancel, confirm or outside modal is subsequently clicked 
-  // 1. create a useState hook to manage the state of modalIsOpen  
+  // WHAT DO WE WANT TO ACHIEVE?
+  // we only want the modal, or background, to be open if any delete button is clicked
+  // we want the modal and beackground to work together, their state should always be the same
+  // their initial state should be closed (open === false)
+  // if they are open, we want to be able to close both whenever a user clicks on the cancel or confirm button, or anywhere outside of the modal 
+  
+  // WHAT TOOLS WILL WE USE?
+  // use the React Hook function 'useState' in combination with a click event handler
+  
+  // DEAL WITH THE MODAL FIRST - modalIsOpen
+  // 1. directly inside of the Todo componnent function code block, create a useState hook and set the initial open state to false, 
+  // 2. use array destructuring to declare two varibles that will store the element array will return from our useState function.
+        // modalIsOpen :to conditionally render the modal based on currently stored state value
+        // setModalIsOpen :the function that will execute to assign a new state value when an argument is passed  
   // 2. set the initial state to (false) 
   const [ modalIsOpen, setModalIsOpen ] = useState(false); 
   
-  // create a function to handle the change of state to true when the delete button is clicked 
+  // create a click event handler function to set an open state
+  // inside the code block, have the function execute our setModalIsOpen function
+  // passing in 'true' as the argument to this setModalIsOpen function so as to set state to that value
   function deleteHandler() {
     setModalIsOpen(true);
   }
 
-  // create a function to handle the change of state back to false when we want to close the modal 
+  // // create a click event handler function to close the modal, setting state value to 'false'
   function closeModalHandler() {
     setModalIsOpen(false);
   }
@@ -24,12 +37,13 @@ function Todo(props) {
     <div className="card">
         <h2>{props.text}</h2>
         <div className="actions">
+          {/* pass in our deleteHandler function to OPEN the modal */}
           <button className="btn" onClick={deleteHandler}>Delete</button>
         </div>
-        {/* boolean test to establish if both the modelIsOpen value is true and the Component function is true. If both are true render the modal.
-        if either are false, do not render the modal. closeModalHandler will change state to false if either the cancel or confirm buttons are clicked. */}
+        {/* use a conditional operator as a boolean test to establish if both the 'modelIsOpen value' is 'true' and the 'closeModalHandler' is 'true'. If both are true the modal is rendered, if either are false, the modal should not be rendered. */}
+        {/* pass in our closeModalHandler to each of the buttons in the modal and onto the backdrop to CLOSE the modal */}
         {modalIsOpen && (
-        <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} /> )}
+                <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} /> )}
         {modalIsOpen && <Backdrop onCancel={closeModalHandler} /> }
       </div>
     );
